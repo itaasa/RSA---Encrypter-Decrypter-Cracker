f2515b0e9fd947fe552bf2a6dd83031403a4ff84
= <=>
2018-05-06 01:53:25 +0000

    Implemented finding block len

--- plaintext.c
+++ plaintext.c
@@ -9,21 +9,27 @@
 int handle_args (char *, int);		//Checks if arguments are valid	
 char* read_plaintext (char *);		//Reads plain text message from file
 int* convert_num (char *);
-
+int* convert_block (int *, int );
+int get_block_size (int );
+int get_num_of_digits(int );
+int get_block_len(int, int`);
 int main (int argc, char ** argv) {
 
 	int n = atoi(argv[2]);
+	int* numtext;
 	char* filepath = argv[1];
 
 	if (handle_args(filepath, n) == 1){
-		printf ("File will be read from: %s\n", argv[1]);	
-		printf ("The public key is: %d\n", n);
-		printf ("Reading from file...\n");
+
 		char* alphatext = read_plaintext(argv[1]);
-		int* numtext = convert_num(alphatext);
+		numtext = convert_num(alphatext);
+		
+		printf ("%d\n", get_block_size(n));
 	}
 }
 
+//Checks if file does not exist, or if the public key is too small
+//Returns 1 if criteria is met, else -1
 int handle_args(char* filepath, int n){
 	
 	FILE* file = fopen (filepath, "r");
@@ -73,14 +79,47 @@ char* read_plaintext(char *filepath) {
 int* convert_num (char *plaintext) {
 	int len = strlen (plaintext);
 
-	int numplain [len];
+	int* numplain = malloc (len);
 
 	int i;
 	for (int i=0; i<len; i++){
-		numplain[i] = (int) plaintext [i];	
-		printf("%d\n", numplain[i]);
+		numplain [i] = (int) plaintext [i];	
+	}	
+
+	return numplain;
+}
+
+int* convert_block (int* num_text, int block_size) {
+
+}
+
+int get_block_size(int n) {
+
+	int min, keydigits = get_num_of_digits(n);
+	
+	min = keydigits / 3;
+	
+	return min;	
+	
+}
+
+int get_block_len (int block_size, int len) {
+
+	if (len % block_size == 0)
+		return len / block_size;
+	else
+		return (len / block_size) + 1
+}
+
+int get_num_of_digits(int n) {
+
+	int count = 0;
+	
+	while (n > 0) {	
+		count++;
+		n /= 10;
 	}
 
-	
+	return count;	
 }
 
